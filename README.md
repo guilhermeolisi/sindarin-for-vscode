67102713857c0909762e7571e9f6fecbf26c54dc
Guilherme Oliveira Siqueira <guilhermequi@yahoo.com.br>
2021-10-28 15:27:58 -0300

    acrecentei o botão update

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![Installs](https://vsmarketplacebadge.apphb.com/installs/goSiqueira.sindarin-for-vscode.svg?color=orange)](https://marketplace.visualstudio.com/items?itemName=goSiqueira.sindarin-for-vscode)
 [![Downloads](https://vsmarketplacebadge.apphb.com/downloads/goSiqueira.sindarin-for-vscode.svg?color=orange)](https://marketplace.visualstudio.com/items?itemName=goSiqueira.sindarin-for-vscode)
 
-[Sindarin for VS Code](https://github.com/chartjs/Chart.js) for [VSCcode](https://code.visualstudio.com/)
+[Sindarin for VS Code](https://github.com/guilhermeolisi/sindarin-for-vscode) for [VSCcode](https://code.visualstudio.com/)
 
 Sindarin for Visual Studio Code is an extension that includes some features to make the Sindarin program easier to use with this powerful text editor.
 
@@ -16,24 +16,24 @@ Sindarin is a crystal diffraction calculation program that uses a dedicated scri
 
 The extension enables Sindarin code highlighting. Sindarin "language" is automatically enabled by the extension when the file with the ".sin" extension is opened in the editor or it can be enabled manually by selecting the "language mode selector" in the status bar. 
 
-![Sindarin Code Highlighting](resources/SindarinCodeHighlithing.png)
+![Sindarin Code Highlighting](https://github.com/guilhermeolisi/sindarin-for-vscode/blob/master/resources/SindarinCodeHighlithing.png?raw=true)
 
-![Sindarin Language in language mode selector](resources/SindarinStatusBarLanguage.png)
+![Sindarin Language in language mode selector](https://github.com/guilhermeolisi/sindarin-for-vscode/blob/master/resources/SindarinStatusBarLanguage.png?raw=true)
 
 2 commands are enable:
 * Interpet: run sindarin to interpret for the current sindarin file (Shortcut: Ctrl+Alt+Down Arrow)
 * Walk: run sindarin to interpret and run optimization routine for the current sindarin file (Shortcut: Ctrl+Alt+Right Arrow)
 
 These commands can be acessed by Command Pallete, short cut or two dedicated icons in editor title. 
-![Sindarin commands in Command Pallete](resources/SindarinCommands.png)
+![Sindarin commands in Command Pallete](https://github.com/guilhermeolisi/sindarin-for-vscode/blob/master/resources/SindarinCommands.png?raw=true)
 
-![Sindarin icons in editor title](resources/SindarinIcons.png)
+![Sindarin icons in editor title](https://github.com/guilhermeolisi/sindarin-for-vscode/blob/master/resources/SindarinIcons.png?raw=true)
 
 These commands are automatically enabled with the Sindarin language signed text
 
 Also, the [Chart.js Preview](https://github.com/chartjs/Chart.js) extension is installed together with the sindarin extension. You can view in VS Code the diffractograms of the .chart.json5 file generated after the sindarin commands
 
-![View diffraction in VS Code wit Chart.js](resources/SindarinDiffractionChart.js.png)
+![View diffraction in VS Code wit Chart.js](https://github.com/guilhermeolisi/sindarin-for-vscode/blob/master/resources/SindarinDiffractionChart.js.png?raw=true)
 
 ## Requirements
 
@@ -42,7 +42,7 @@ the program needs to be in the default folder on your computer:
 * Windows: c:\Sindarin
 * Linux and macOS: ~/Sindarin
 
-If you want another folder, you need to include the path of this folder in the PATH environment variable in your operating system to allow the extension to find the Sindarin program. 
+If you want other folder, you need to include the path of this folder in the PATH environment variable in your operating system to allow the extension to find the Sindarin program. 
 
 
 **Good Work!**
--- src/extension.ts
+++ src/extension.ts
@@ -25,74 +25,107 @@ export async function activate(context: vscode.ExtensionContext) {
 	getOutputChannel().appendLine("Start Sindarin extension");
 	await VerifyOS();
 
+	let updateCommand = vscode.commands.registerCommand('sindarin-for-vscode.update', () => {
+		InterpreterCommand("update");
+	});
+	context.subscriptions.push(updateCommand);
+
 	let interpretCommand = vscode.commands.registerCommand('sindarin-for-vscode.interpret', () => {
-		//vscode.window.showInformationMessage('Hello World from Sindarin for vscode!');
 		InterpreterCommand("interpret");
 	});
 	context.subscriptions.push(interpretCommand);
 
 	let walkCommand = vscode.commands.registerCommand('sindarin-for-vscode.walk', () => {
-		//vscode.window.showInformationMessage('Hello World from Sindarin for vscode!');
 		InterpreterCommand("walk");
 	});
-
 	context.subscriptions.push(walkCommand);
-	
+
+
 }
 // this method is called when your extension is deactivated
 export function deactivate() { }
 
-async function InterpreterCommand(mode:string): Promise<void> {
+async function InterpreterCommand(mode: string): Promise<void> {
 	var commandLinePower: string; // = "Sindarin";
 
-		if (hasSinEnv) {
-			commandLinePower = "Sindarin";
+	if (hasSinEnv) {
+		commandLinePower = "Sindarin";
+	}
+	else if (pathSin) {
+		commandLinePower = path.join(pathSin, "Sindarin");
+	}
+	else {
+		if (OSIndex === 0) {
+			vscode.window.showInformationMessage('Sindarin program is not found in default folder: /Sindarin');
 		}
-		else if (pathSin) {
-			commandLinePower = path.join(pathSin, "Sindarin");
+		else if (OSIndex === 1) {
+			vscode.window.showInformationMessage('Sindarin program is not found in default folder: C:\\Sindarin');
+		}
+		else if (OSIndex === 2) {
+			vscode.window.showInformationMessage('Sindarin program is not found in default folder: /Sindarin');
 		}
 		else {
-			if (OSIndex === 0) {
-				vscode.window.showInformationMessage('Sindarin program is not found in default folder: ~/Sindarin');
-			}
-			else if (OSIndex === 1) {
-				vscode.window.showInformationMessage('Sindarin program is not found in default folder: C:\\Sindarin');
-			}
-			else if (OSIndex === 2) {
-				vscode.window.showInformationMessage('Sindarin program is not found in default folder: /Sindarin');
-			}
-			else {
-				//TODO mesnagem que não reconheceu o commandline do OS
-			}
-			return;
+			//TODO mesnagem que não reconheceu o commandline do OS
 		}
+		return;
+	}
 
-		const file = vscode2.window.activeTextEditor.document.uri.fsPath;
+	const file = vscode2.window.activeTextEditor.document.uri.fsPath;
 
-		//https://blog.heshamamin.com/2009/04/calling-powershell-script-in-path-with.html
-		//const commandLinePower = "C:\\\'SD Card\'\\Guilherme\\Documentos\\Acadêmico\\VisualStudio\\Sindarin\\Sindarin\\bin\\Release\\net5.0\\publish\\windows64\\Sindarin.exe";
+	//https://blog.heshamamin.com/2009/04/calling-powershell-script-in-path-with.html
+	//const commandLinePower = "C:\\\'SD Card\'\\Guilherme\\Documentos\\Acadêmico\\VisualStudio\\Sindarin\\Sindarin\\bin\\Release\\net5.0\\publish\\windows64\\Sindarin.exe";
 
-		if (path.extname(file).toUpperCase() !== '.SIN') {
-			//TODO enviar mensagem para terminal dizendo que o arquivo não é no formato apropriado
-			return;
-		}
-		const fileName = path.basename(file);
-		const folder = path.dirname(file);
-		const options: vscode.ShellExecutionOptions = {
-			cwd: folder,
-		};
-		const definition: vscode.TaskDefinition = {
-			type: "shell"
-		};
+	if (path.extname(file).toUpperCase() !== '.SIN') {
+		//TODO enviar mensagem para terminal dizendo que o arquivo não é no formato apropriado
+		return;
+	}
+	const fileName = path.basename(file);
+	const folder = path.dirname(file);
+	const options: vscode.ShellExecutionOptions = {
+		cwd: folder,
+	};
+	const definition: vscode.TaskDefinition = {
+		type: "shell"
+	};
 
-		// const taskTemp = new vscode.Task(definition, vscode.TaskScope.Workspace, `TestTask ${definition.number}`, definition.type,
-		// 	new vscode.ShellExecution("\"& {&\'" + commandLinePower + "\' " + fileName + " --walk --all --json " + "}\"", options), [""]);
-		const taskTemp = new vscode.Task(definition, vscode.TaskScope.Workspace, `Sindarin ${mode}`, definition.type,
-			new vscode.ShellExecution(commandLinePower, [fileName, "--" + mode, "--all", "--json"], options), [""]);
-		vscode.tasks.executeTask(taskTemp);
+	// const taskTemp = new vscode.Task(definition, vscode.TaskScope.Workspace, `TestTask ${definition.number}`, definition.type,
+	// 	new vscode.ShellExecution("\"& {&\'" + commandLinePower + "\' " + fileName + " --walk --all --json " + "}\"", options), [""]);
+	var taskTemp: vscode.Task;
+	if (mode !== 'update') {
+		taskTemp = new vscode.Task(definition, vscode.TaskScope.Workspace, `Sindarin ${mode}`, definition.type,
+			new vscode.ShellExecution(commandLinePower, [fileName, "--" + mode, "--all", "--code"], options), [""]);
+	}
+	else {
+		taskTemp = new vscode.Task(definition, vscode.TaskScope.Workspace, `Sindarin ${mode}`, definition.type,
+			new vscode.ShellExecution(commandLinePower, ["--" + mode, "--code"], options), [""]);
+	}
+	vscode.tasks.executeTask(taskTemp);
+
 }
 
 async function VerifyOS(): Promise<void> {
+	
+	//https://natclark.com/tutorials/javascript-check-operating-system/
+	// let os = `Undetected`;
+	// if (vscode2.appVersion.indexOf("Win") !== -1) {
+	// 	//os = `Windows`;
+	// 	OSIndex = 0;
+	// }
+	// else if (vscode2.appVersion.indexOf("Linux") !== -1) {
+	// 	//os = `Linux`;
+	// 	OSIndex = 1;
+	// }
+	// else if (vscode2.appVersion.indexOf("Mac") !== -1) {
+	// 	//os = `MacOS`;
+	// 	OSIndex = 2;
+	// }
+	// else if (vscode2.appVersion.indexOf("X11") !== -1) {
+	// 	//os = `UNIX`;
+	// 	OSIndex = 3;
+	// }
+	// return os;
+
+
 	const options: vscode.ShellExecutionOptions = {
 
 	};
@@ -122,7 +155,7 @@ async function VerifyOS(): Promise<void> {
 				verifySin = false;
 			}
 			if (verifySin) {
-				pathSin = "~/Sindarin";
+				pathSin = "/Sindarin";
 			}
 			else {
 				vscode.window.showInformationMessage('Sindarin program is not found in default folder: ~/Sindarin');
@@ -163,6 +196,7 @@ async function VerifyOS(): Promise<void> {
 			else {
 				//Fazer um comando para verificar se é macOS
 				OSIndex = 2;
+				pathSin = "/Sindarin";
 			}
 		}
 	}
